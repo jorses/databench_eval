080cd804df76459d2250aa2f9aa70fe5fb0e69cc
jorses <15893473+jorses@users.noreply.github.com>
2025-01-21 11:39:48 +0100

    v3.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="databench_eval",
-    version="3.0.0",
+    version="3.0.1",
     author="jorses",
     author_email="jorgeosesgrijalba@gmail.com",
     description="Evaluation framework for DataBench",
